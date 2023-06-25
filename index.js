b2e4ee7372026ba5ec7ea9851e791d3336f9c4af
Raymond <oetaray@gmail.com>
2023-06-25 16:45:43 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(_event = 'roller-skate'){
+    return "This Saturday I will ${_event}."
+}
+function mondayWork(_event = "go to the office"){
+    return "This Monday we will ${_event}"
+}
+function wrapAdjective(flair = `*`){
+    return function (Adjective = `special`){
+        return `You are ${flair}${Adjective}${flair}!`
+    }
+}
+wrapAdjective("%")("a dedicated programmer"); //=> "you are %a dedicated programmer%!"
